0fdae28c4e0703750712cc763e0adc8d89247ca6
Mary Ondra <vidra48@mail.ru>
2020-08-07 12:48:14 +0300

    added ignored files to sourse control

---
+++ FinalTask/Application/Utility/Logs/Utility.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace Application.Logs
+{
+    public class Utility
+    {
+        private static int count = 0;
+
+        public static void VerifyDir(string path)
+        {
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(path);
+                if (!dir.Exists)
+                {
+                    dir.Create();
+                }
+            }
+            catch { }
+        }
+
+        public static void Logger(string message, string methodLocation)
+        {
+            string path = @"C:\Users\Maria\Source\Repos\ITAcademy_console_TETRIS\FinalTask\Application\Utility\Logs\";
+
+            string fileName = "log" + System.DateTime.Today.ToString("yyyyMMdd") + $"_[{count}]" + ".txt";
+
+            string lines = $"[{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToShortDateString()}]" +
+                $"[{methodLocation}] : [{message}]";
+
+            VerifyDir(path);
+
+            try
+            {
+                StreamWriter file = new System.IO.StreamWriter(path + fileName, true);
+                file.WriteLine(lines);
+                file.Close();
+            }
+            catch (Exception) { }
+        }
+    }
+}
+
+
